04449211fcea1507bf8870696e365cbdd63ab97d
tangj <840391701@qq.com>
2018-03-07 18:30:21 +0800

    redis BO类文件

--- src/main/java/com/janti/service/ISeckillUserService.java
+++ src/main/java/com/janti/service/ISeckillUserService.java
@@ -1,8 +1,12 @@
 package com.janti.service;
 
+import com.janti.domain.UserVo;
+
 /**
  * @author tangj
  * @date 2018/3/6 22:29
  */
 public interface ISeckillUserService {
+
+    UserVo login(String userId,String password);
 }
--- src/main/java/com/janti/service/impl/SeckillUserService.java
+++ src/main/java/com/janti/service/impl/SeckillUserService.java
@@ -1,6 +1,9 @@
 package com.janti.service.impl;
 
+import com.janti.domain.UserVo;
+import com.janti.exception.TipException;
 import com.janti.service.ISeckillUserService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,4 +12,12 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SeckillUserService implements ISeckillUserService{
+    @Override
+    public UserVo login(String userId, String password) {
+        if (StringUtils.isBlank(userId)|| StringUtils.isBlank(password)){
+            throw new TipException("用户名和密码为空");
+        }
+
+        return null;
+    }
 }
